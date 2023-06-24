5c67ec7e9e4b6f5b815c77b6f73e7e20fc1f603e
kristof1345 <kristofnemeth1345@gmail.com>
2023-06-24 09:45:38 +0200

    copy url

--- client/src/components/Survee.jsx
+++ client/src/components/Survee.jsx
@@ -2,10 +2,30 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Survee = ({ survey }) => {
+  const copyURL = async (e) => {
+    let text = e.target.innerText;
+    try {
+      await navigator.clipboard.writeText(text);
+      console.log("Content copied to clipboard");
+    } catch (err) {
+      console.error("Failed to copy: ", err);
+    }
+  };
+
   return (
-    <Link className="survey" to={`survey/${survey.SurveyID}`}>
-      <h2 className="survey-title">{survey.Title}</h2>
-    </Link>
+    <div className="survey">
+      <Link to={`survey/${survey.SurveyID}`}>
+        <h2 className="survey-title">{survey.Title}</h2>
+      </Link>
+      <div>
+        Share your survey with others:
+        <br />
+        <button
+          className="copy-btn"
+          onClick={(e) => copyURL(e)}
+        >{`http://localhost:3000/${survey.SurveyID}`}</button>
+      </div>
+    </div>
   );
 };
 
